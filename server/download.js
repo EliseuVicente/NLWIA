3e003a5bd252d5566726b8392c6a67a4c6aea916
Eliseu Vicente <eliseu965@gmail.com>
2023-09-17 23:01:24 -0300

    creating function to download videos

--- server/download.js
+++ server/download.js
@@ -1,7 +1,7 @@
 import ytdl from 'ytdl-core'
 import fs from 'fs'
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise ((resolve, reject) => {
   const videoURL = "https://www.youtube.com/shorts/" + videoId
   console.log("Realizando o download do video:" + videoId)
 
@@ -15,10 +15,13 @@ export const download = (videoId) => {
   })
   .on("end", () =>{
     console.log("Download do vídeo finalizado.")
+    resolve()
   })
   .on("error", (error) =>{
     console.log("Não foi possivel fazer o download do vídeo. Detalhes do erro:",
      error
     )
-  }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+    reject(error)
+  })
+  .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+})
